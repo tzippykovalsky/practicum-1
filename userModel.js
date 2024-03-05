2422ece66f48223e1e52dd0ccc341ae1795c0727
braindi <77191b.com>
2024-03-05 10:04:46 +0200

    create validator

--- userModel.js
+++ userModel.js
@@ -1,12 +1,32 @@
 import mongoose from "mongoose";
+import Joi from "joi";
 
 const userSchema = mongoose.Schema({
-  
-    userName: String,
-    phone: String,
-    email: { type: String, unique: true }
+
+    userName: { type: String, require: true },
+    phone: { type: String, require: true },
+    email: { type: String, unique: true, require: true }
 
 });
 
 export const User = mongoose.model("users", userSchema);
 
+export const userValidator = (userToValidate) => {
+    let user = Joi.object({
+        userName: Joi.string().required(),
+        email: Joi.string().email().custom((value, helpers) => {
+            if (value.includes(' ')) {
+                return helpers.error('any.invalid');
+            }
+            return value;
+        }).required(),
+        phone: Joi.string().required()
+    })
+}
+
+export const userValidatorWithOutEmail = (userToValidate) => {
+    let user = Joi.object({
+        userName: Joi.string().required(),
+        phone: Joi.string().required()
+    })
+}
